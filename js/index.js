4405adab94e30b942d37b8220548754ebec024f7
Romaric Zounlome <rzounlom@gmail.com>
2023-09-17 09:58:58 -0400

    complete rating functionality

--- js/index.js
+++ js/index.js
@@ -2,24 +2,27 @@
 let optionSelected = "1";
 
 // select and add click event listeners to each option
-const options = document
-  .querySelectorAll(".option")
-  .forEach((option) =>
-    option.addEventListener(
-      "click",
-      () =>
-        (optionSelected = option.innerHTML) && console.log({ optionSelected })
-    )
-  );
+const options = document.querySelectorAll(".option");
+
+options.forEach((option) =>
+  option.addEventListener("click", () => {
+    //find active option and remove active class
+    const activeOption = options[parseInt(optionSelected) - 1];
+    activeOption.classList.remove("active");
+
+    //add active class to selected option
+    optionSelected = option.innerHTML;
+    option.classList.add("active");
+  })
+);
 
 //create functino to for submit
 function onSubmit() {
-  console.log("form submitted");
   //query for ratings cards and destructure them into variables
   const ratingCard = document.querySelector(".rating-card");
   const thankYouCard = document.querySelector(".thank-you-card");
   const submittedRating = document.querySelector(".selection span");
-  console.log({ submittedRating });
+
   //toggle active class
   ratingCard.classList.remove("active");
   thankYouCard.classList.add("active");
